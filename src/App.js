71461ec0289cc6f7ab5357e906034e3f52b51773
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-07-28 11:35:39 +0500

    Added Soundwave animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Navbar, Landing, Purpose } from './components/index';
+import { Navbar, Landing, Purpose, Soundwave } from './components/index';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Landing />
+      <Soundwave />
       <Purpose />
     </div>
   );
