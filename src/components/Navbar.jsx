5bf404bf0a1afe4a544334df085ddb963596f94f
Aqib Naeem <maqibnaeem2019@gmail.com>
2022-07-28 10:06:13 +0500

    Added logo through Fontawesome svg library

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='nav'>
       <div className="nav-container">
-        <h1 className="nav-logo">Logo</h1>
+        <p> <i class="fa-solid fa-sun-plant-wilt"></i> </p>
         <div className="nav-buttons-container">
           <button className="signup">Signup</button>
           <button className="signup">Signin</button>
